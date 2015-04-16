99d7e0afefe7a59cfcdff4140c0283f547054f6b
Jacob Garcia <jacobgravesthefiend@gmail.com>
2015-04-15 18:17:11 -0700

    4/15/15 2:
    Fixed the error I had by switching 'tasks' to 'todo'

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 // localhost is first because it's choosing that as the connectin base
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 // If database connection fails, then we want it to die and display the error message
 if ($mysqli->connect_error) {
 	die('Connect Error (' . $mysqli->connect_errno - ')'
